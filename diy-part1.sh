77d13667b0dad0ddb5b834f2f4e64f59ba7a7e11
zhlshmtu <zhlshmtu@gmail.com>
2025-08-11 15:21:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,5 @@
 
 # Add a feed source
 sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '2i src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
-sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
